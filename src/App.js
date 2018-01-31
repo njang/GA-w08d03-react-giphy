2e0be6e1ee754c39e21612ba8e4ee6fbbaddc8b0
njang <neo.w.jang@gmail.com>
2018-01-31 10:16:27 -0600

    Complete step 1

--- src/App.js
+++ src/App.js
@@ -1,1 +1,14 @@
 // Developer TODO: Define App component defintion here
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      	<h1>Hello World</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 // Developer TODO: Load in App component and render to the DOM
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(
+		<App />
+, document.getElementById('root'));
+// registerServiceWorker();
