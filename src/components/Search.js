dd84b3a14fcc1fc751709752ec2c7effdaf099fb
njang <neo.w.jang@gmail.com>
2018-01-31 11:14:00 -0600

    Complete form change function

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
 
 class Search extends Component {
-  handleButtonClick(e) {
+	constructor(props){
+    super(props)
+    this.state = {value: ""}
+
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+    this.handleFormInput = this.handleFormInput.bind(this);
+  }
+  handleFormSubmit(e) {
     // It is good practice to still prevent default behavior
     e.preventDefault();
     console.log("Searched!");
+  }
+  handleFormInput(e) {
+    // It is good practice to still prevent default behavior
+    // e.preventDefault();
+    this.setState({ value: e.target.value });
+    console.log("Inputted!");
   }
 
   render() {
     return (
       <div className="Search">
-      	<form>
-      		<input type="text" />
-      		<button type="submit" onClick={(e) => this.handleButtonClick(e)}>Search</button>
+      	<form onSubmit={ this.handleFormSubmit } >
+      		<input type="text" value={ this.state.value } onChange={ this.handleFormInput } />
+      		<button type="submit" >Search</button>
       	</form>
       </div>
     );
