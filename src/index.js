70c9256bfefbf8a515bf5daeb2969fa0b5357c55
njang <neo.w.jang@gmail.com>
2018-01-31 10:20:55 -0600

    Complete step 2

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-// Developer TODO: Load in App component and render to the DOM
+// Developer TODO: Load in Home component and render to the DOM
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './Home';
 // import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-		<App />
+		<Home />
 , document.getElementById('root'));
 // registerServiceWorker();
