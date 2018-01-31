fe2ea759831e785bd0c209ed4c3e9a0ecca2d617
njang <neo.w.jang@gmail.com>
2018-01-31 10:33:18 -0600

    Complete step 3

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Search from './components/Search';
 
 class Home extends Component {
   render() {
     return (
       <div className="Home">
       	<h1>Hello F-ing World</h1>
+      	<Search />
       </div>
     );
   }
